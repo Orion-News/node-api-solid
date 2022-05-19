4aafb11b7fe998f176d23e8a86c312fa9513fbd5
Orion-News <geeh-eb@hotmail.com>
2022-05-19 16:04:10 -0300

    feat: ensure integration module to send email and create class to mailtrap

---
+++ src/repositories/implementations/MailtrapMailProvider.ts
@@ -0,0 +1,33 @@
+import { MailProvider, Message } from "../../providers/MailProvider";
+import nodemailer from 'nodemailer'
+import Mail from "nodemailer/lib/mailer";
+
+export class MailtrapMailProvider implements MailProvider {
+  private transporter: Mail;
+
+  constructor () {
+    this.transporter = nodemailer.createTransport({
+      host: "smtp.mailtrap.io",
+      port: 2525,
+      auth: {
+        user: "5feeafea53958e",
+        pass: "339ecbd8aa9cb6"
+      }
+    })
+  }
+
+  async sendMail(message: Message): Promise<void> {
+    await this.transporter.sendMail({
+      to: {
+        address: message.to.email,
+        name: message.to.name,
+      },
+      from: {
+        name: message.from.name,
+        address: message.from.email
+      },
+      subject: message.subject,
+      html: message.body
+    })
+  }
+}
