3b20822d6eb179b3eef7771a35f69b81c81e8c3a
Orion-News <geeh-eb@hotmail.com>
2022-05-19 15:38:42 -0300

    feat: ensure send mail

--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,10 +1,12 @@
 import { User } from "../../entities/User";
+import { MailProvider } from "../../providers/MailProvider";
 import { UsersRepository } from "../../repositories/UsersRepository";
 import { CreateUserRequestDTO } from "./CreateUserDTO";
 
 export class CreateUserUseCase {
   constructor (
-    private usersRepository: UsersRepository
+    private usersRepository: UsersRepository,
+    private mailProvider: MailProvider
   ) {}
   
   async excute(data: CreateUserRequestDTO) {
@@ -17,5 +19,18 @@ export class CreateUserUseCase {
     const user = new User(data);
     
     await this.usersRepository.save(user);
+
+    await this.mailProvider.sendMail({
+      to: {
+        name: data.name,
+        email: data.email
+      },
+      from: {
+        name: 'Gevs',
+        email: 'Gevs@meuapp.com'
+      },
+      subject: 'seja Bem-Vindo á Plataform',
+      body: '<p>Você já pode fazer login em nossa plataforma. </p>'
+    })
   }
 }
